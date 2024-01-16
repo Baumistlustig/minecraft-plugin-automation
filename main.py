06f02cf905de2f23607beeecc7f361427ba27ed3
Johannes Hölwlerth <johannes.hoellwerth@protonmail.com>
2024-01-16 10:46:11 +0100

    Moved checking config into seperat function

--- main.py
+++ main.py
@@ -13,61 +13,63 @@ configurationFile.close()
 
 working_path = config['path']
 
+def checkConfig(config):
+    # Check current forconfiguration
+    if not os.path.isfile(config['path'] + 'pom.xml'):
+        print('\033[91m' + '\033[1m' + 'Path is not valid!' + '\033[0m')
+
+        return False
+
+    if config['server']['local']:
+        if not os.path.isdir(config['server']['serverPath'] + 'plugins'):
+            print('\033[91m' + '\033[1m' + 'Local server path is not valid!' + '\033[0m')
+            
+            return False
+    else:
+        # Try to connect to server
+        ftp = FTP(
+            config['server']['adress'] + ':' + config['server']['port'],
+            config['server']['username'],
+            config['server']['password']
+        )
+
+        if not ftp.login():
+            print('\033[91m' + '\033[1m' + 'Could\'nt connect to remote server!' + '\033[0m')
+
+            return False
+        
+    return True
+
 @app.command()
 def configure(
     path: Annotated[Optional[str], typer.Option()] = working_path,
     local: Annotated[Optional[bool], typer.Option()] = config['server']['local'],
-    localServerPath: Annotated[Optional[str], typer.Option()] = config['server']['localServerPath'],
+    serverPath: Annotated[Optional[str], typer.Option()] = config['server']['serverPath'],
     adress: Annotated[Optional[str], typer.Option()] = config['server']['adress'],
     port: Annotated[Optional[int], typer.Option()] = config['server']['port'],
     username: Annotated[Optional[str], typer.Option()] = config['server']['username'],
     password: Annotated[Optional[str], typer.Option()] = config['server']['password'],
     method: Annotated[Optional[str], typer.Option()] = config['server']['method']):
 
-    file = open('config.json', 'w')
-
-    # Validate input
-    if not os.path.isfile(path + 'pom.xml'):
-        print('\033[91m' + '\033[1m' + 'Path is not valid!' + '\033[0m')
-
-        file.write(json.dumps(config))
-
-        return
-    
-    if local:
-        if not os.path.isdir(localServerPath + 'plugins'):
-            print('\033[91m' + '\033[1m' + 'Local server path is not valid!' + '\033[0m')
-            
-            return
-    else:
-        # Try to connect to server
-        ftp = FTP(
-            adress + ':' + port,
-            username,
-            password
-        )
-
-        if not ftp.login():
-            print('\033[91m' + '\033[1m' + 'Server login is not valid!' + '\033[0m')
-
-            return
-
-    file.write(json.dumps({
+    file = open('config.json')
+    if checkConfig(json.load(file)):
+        writeFile = open('config.json', 'w')
+        writeFile.write(json.dumps({
         "path": path,
         "server": {
             "local": local,
-            "localServerPath": localServerPath,
+            "serverPath": serverPath,
             "adress": adress,
             "port": port,
             "username": username,
             "password": password,
             "method": method
-        }
-    }))
+        }}))
+        writeFile.close()
 
+        print('\033[92m' + '\033[1m' + 'Saved configuration' + '\033[0m')
+    
     file.close()
-
-    print('\033[92m' + '\033[1m' + 'Saved configuration ' + u'\u2713' + '\033[0m')
 
 if __name__ == '__main__':
     app()
